22ff830bb2b2382c106f536d7bc27e52ca0f4a56
Ricardo <ricardo.br.pi@gmail.com>
2021-04-30 18:41:10 -0300

    feat: add the jquery library

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-
-function btn_hamburguer() {
-  if (document.getElementsByClassName("disabled")) {
-    console.log("romeve disabled")
-    document.getElementById("btn_primary").classList.add('active');
-    document.getElementById("btn_primary").classList.remove('disabled');
-
-  }
-  else {
-    console.log("remove active")
-    document.getElementById("btn_primary").classList.remove('active');
-  }
-};
+$('.btn').click(function () {
+  console.log("helo")
+  $('#btn_primary').toggleClass('active');
+});
